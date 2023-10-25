2046b44757c2af49a93bcf45baf2e43aa8392d21
Alex <137710414+SelinAlex@users.noreply.github.com>
2023-10-25 23:32:40 +0300

    added return

--- js/task-3.js
+++ js/task-3.js
@@ -5,6 +5,7 @@ function getElementWidth(content, padding, border) {
     const borderWidth = parseFloat(border);
     
     const totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;   
+     return totalWidth;
 }
 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
